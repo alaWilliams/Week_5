7396a7cf8264d699339af3722fe9477ac229d1e1
alaWilliams <95369346+alaWilliams@users.noreply.github.com>
2024-09-30 14:38:21 +0300

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,23 @@
+// Create a program that keeps asking the user to input numbers. These numbers are stored in an array. Keep asking till zero is entered. After that the numbers are printed out in a reverse order. Don’t use array methods for this but only simple loops.
+
+process.stdout.write('This program reads numbers from the keyboard.\nAfter receiving a zero, it prints the numbers in reverse order. Please, start entering numbers.\nThe program will stop when you enter zero.\n')
+
+let numbersArray = new Array();
+
+process.stdout.write(numbersArray.length + ' Enter a number: ')
+process.stdin.on('data', function(input) {
+  let number = Number(input);
+  if (isNaN(number)) {
+    process.stdout.write('Please, enter a number.');
+  } else if(number === 0) {
+      numbersArray.push(number);
+      process.stdout.write('Reverse order:\t')
+      for (let i = numbersArray.length-1; i >= 0; i--) {
+        process.stdout.write( numbersArray[i] + ' ');
+      }
+      process.exit()
+    } else {
+        numbersArray.push(number);
+        process.stdout.write(numbersArray.length + ' Enter a number: ');
+    }
+})
