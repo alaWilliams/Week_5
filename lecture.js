3864c3b1c648ce449c5041402fe7e0cff46395ae
alaWilliams <95369346+alaWilliams@users.noreply.github.com>
2024-09-30 09:05:04 +0300

    Add files via upload

---
+++ lecture.js
@@ -0,0 +1,8 @@
+let fruit = ["banana", "apple", "kiwi", "orange"];
+
+
+process.stdout.write("First item in the array is " + fruit[0] + '.\n\n')
+
+for (let i = fruit.length-1; i >= 0; i--) {
+  process.stdout.write(fruit[i] + '\n')
+}
